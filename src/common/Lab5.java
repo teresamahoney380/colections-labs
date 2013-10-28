fc658e963fc23dc333d398c177377ede5d6c7288
Owner <Owner@10.30.17.199>
2013-10-28 18:45:45 -0500

    completed lab 5

--- src/common/Lab5.java
+++ src/common/Lab5.java
@@ -14,7 +14,7 @@ public class Lab5 {
     public static void main(String[] args) {
         Employee e1 = new Employee("Smith", "Albert", "111-11-1111");
         Employee e2 = new Employee("Jones", "Grace", "222-22-2222");
-        Employee e3 = new Employee("Smith", "Albert", "111-11-1111");
+        Employee e3 = new Employee("Smith", "Albert2", "111-11-1111");
         Employee e4 = new Employee("Garcia", "Maria", "333-33-3333");
         
         Map<String,Employee> employeeMap= new HashMap<String,Employee>();
@@ -22,5 +22,19 @@ public class Lab5 {
         employeeMap.put("222-22-2222", e2);
         employeeMap.put("111-11-1111", e3);
         employeeMap.put("333-33-3333", e4);
+     
+        // shows that duplicate key overwrote the first values
+      Employee e = (Employee)employeeMap.get("111-11-1111");
+        System.out.println("Retrieving employee with ssn: 111-11-1111: " + e);
+        
+        //
+        // loop thru object
+        Set keys = employeeMap.keySet();
+        for(Object key : keys) {
+            Employee found = (Employee)employeeMap.get(key);
+            System.out.println(found.toString());
+        }
     }
-}
+        
+    }
+
